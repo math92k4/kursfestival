2d607a49ef5596fee8840aaa981740cd815a7348
Mathias <math92k4@stud.kea.dk>
2021-11-18 19:10:44 +0100

    init om-kurs php setup

--- single.php
+++ single.php
@@ -1,5 +1,18 @@
 <?php $pods_name = pods_v( 0, 'url'); ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="../../wp-content/themes/kursfestival/dist/css/app.css">
+    <link rel="icon" href="../../wp-content/themes/kursfestival/imgs/ellipse_55.png" type="image/x-icon"/>
+   <title><?php echo get_the_title(); ?></title>
+</head>
+<body>
+
+
 <body class=" <?php echo $pods_name ?> ">
 
 <?php
@@ -17,3 +30,4 @@ if ($pods_name == 'om-kurs') {
 ?>
 
 </body>
+</html>
--- theme-parts/om-kurs.php
+++ theme-parts/om-kurs.php
@@ -1,6 +1,3 @@
-<body class="om_kurs">
-    
-
 <?php 
 $params = array(
     'orderby' => 'post_date',
@@ -11,21 +8,49 @@ echo get_the_title();
 
 $menu_pods = pods('om_kurs', $params); ?>
 <nav>
-<ul>
-<?php while ($menu_pods->fetch()) { ?>
-
-
-<li >
-    <a href=" <?php echo $menu_pods->field('permalink') ?> "> <?php echo $menu_pods->field('title') ?> </a>
-</li>
-
-<?php } ?>
-</ul>
+    <ul>
+    <?php while ($menu_pods->fetch()) { ?>
+        <li >
+            <a href=" <?php echo $menu_pods->field('permalink') ?> "> <?php echo $menu_pods->field('title') ?> </a>
+        </li>
+    <?php } ?>
+    </ul>
 </nav> 
 
 <?php
 $id = get_the_ID();
-$post = pods('om-kurs', $params);
+$post = pods('om_kurs', $params);
+if ($post->fetch($id)) {
 
-?> 
-</body>
+    ?>
+    <section>
+
+        <?php
+        // Kun indsæt img, hvis det er indsat i CMS
+        if($post->field('billede')){
+
+        $image = $post->field('billede');
+        $fallback = $post->field('billede_fallback');
+        $size = ''; 
+        $default = -1; 
+        $force = false; 
+        $imgUrl = pods_image_url($image, $size, $default, $force);
+        $fallbackUrl = pods_image_url($fallback, $size, $default, $force);
+        ?>
+
+        <picture>
+            <source srcset=" <?php echo $imgUrl ?>" type="image/webp">
+            <img src=" <?php echo $fallbackUrl ?> " alt="">
+        </picture>
+
+        <?php } ?>
+
+
+        <article>
+            <?php echo $post->field('tekst') ?>
+        </article>
+
+    </section>
+
+<?php }
+
