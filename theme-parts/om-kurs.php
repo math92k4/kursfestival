89966125a72d447abe5cb42308d96d10027769a0
Mathias <math92k4@stud.kea.dk>
2021-11-22 15:33:52 +0100

    responsive om-kurs

--- theme-parts/om-kurs.php
+++ theme-parts/om-kurs.php
@@ -8,52 +8,63 @@ $menu_pods = pods('om_kurs', $params); ?>
 <div class="header_margin"></div>
 
 <main>
-    <section>
-        <nav>
-            <ul>
-            <?php while ($menu_pods->fetch()) { ?>
-                <li >
-                    <a href=" <?php echo $menu_pods->field('permalink') ?> "> <?php echo $menu_pods->field('title') ?> </a>
-                </li>
-            <?php } ?>
-            </ul>
-        </nav> 
-    </section>
-
-    <?php
-    $id = get_the_ID();
-    $post = pods('om_kurs', $params);
-    if ($post->fetch($id)) {
-
-        ?>
-        <section class="om_container">
-
-            <?php
-            // Kun indsæt img, hvis det er indsat i CMS
-            if($post->field('billede')){
-
-            $image = $post->field('billede');
-            $fallback = $post->field('billede_fallback');
-            $size = ''; 
-            $default = -1; 
-            $force = false; 
-            $imgUrl = pods_image_url($image, $size, $default, $force);
-            $fallbackUrl = pods_image_url($fallback, $size, $default, $force);
-            ?>
-
-            <picture>
-                <source srcset=" <?php echo $imgUrl ?>" type="image/webp">
-                <img src=" <?php echo $fallbackUrl ?> " alt="">
-            </picture>
-
-            <?php } ?>
-
-
-            <article>
-                <?php echo $post->field('tekst') ?>
-            </article>
-
+    <h1><?php echo get_the_title(); ?></h1>
+    <div class="wrapper">
+        <section>
+            <nav class="side_menu">
+                <ul>
+                <?php while ($menu_pods->fetch()) { ?>
+                    <li >
+                        <?php if (get_the_title() == $menu_pods->field('title')) { ?>
+                            <a class="current" href=" <?php echo $menu_pods->field('permalink') ?> "> <?php echo $menu_pods->field('title') ?> </a>
+                        <?php } else { 
+                            ?>
+                            <a href=" <?php echo $menu_pods->field('permalink') ?> "> <?php echo $menu_pods->field('title') ?> </a>
+                            <?php
+                        }
+                        ?>
+                        
+                    </li>
+                <?php } ?>
+                </ul>
+            </nav> 
         </section>
+
+        <?php
+        $id = get_the_ID();
+        $post = pods('om_kurs', $params);
+        if ($post->fetch($id)) {
+
+            ?>
+            <section class="om_container">
+
+                <?php
+                // Kun indsæt img, hvis det er indsat i CMS
+                if($post->field('billede')){
+
+                $image = $post->field('billede');
+                $fallback = $post->field('billede_fallback');
+                $size = ''; 
+                $default = -1; 
+                $force = false; 
+                $imgUrl = pods_image_url($image, $size, $default, $force);
+                $fallbackUrl = pods_image_url($fallback, $size, $default, $force);
+                ?>
+
+                <picture>
+                    <source srcset=" <?php echo $imgUrl ?>" type="image/webp">
+                    <img src=" <?php echo $fallbackUrl ?> " alt="">
+                </picture>
+
+                <?php } ?>
+
+
+                <article>
+                    <?php echo $post->field('tekst') ?>
+                </article>
+
+            </section>
+        </div>
     </main>
 
 <?php }
