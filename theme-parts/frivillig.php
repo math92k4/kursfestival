68d475f38228c8503771eb1f4e5654f840fa9c5f
Rasmus <rasm38h8@stud.kea.dk>
2021-11-23 12:56:05 +0100

    form talks with database now

--- theme-parts/frivillig.php
+++ theme-parts/frivillig.php
@@ -1,39 +1,93 @@
 <section>
-
+<div id="form_wrapper">
 <form action="frivillig_signup" id="frivillig_signup">
-<fieldset>
-<legend>Udfyld denne formular for at blive frivillig</legend>
+<fieldset id="frivillig_info">
 <div class="form_ctrl">
-<label for="frivillig_navn">Navn</label>
-<input type="text" name="frivillig_navn">
+<label for="frivillig_navn">Fulde navn</label>
+<input type="text" name="frivillig_navn" minlength="2" placeholder="Frivillig Frivilligsen" id="frivillig_navn">
 </div>
 <div class="form_ctrl">
-<label for="frivillig_email">Email</label>
-<input type="email" name="frivillig_email">
+<label for="frivillig_email">E-mail</label>
+<input type="email" name="frivillig_email" minlength="2" placeholder="frivillig@frivillig.dk" id="frivillig_email">
 </div>
 <div class="form_ctrl">
 <label for="frivillig_telefon">Telefon</label>
-<input type="text" name="frivillig_telefon" maxlength="8">
+<input type="text" name="frivillig_telefon" maxlength="8" placeholder="12345678" id="frivillig_telefon">
 </div>
 <div class="form_ctrl">
-<label for="file">Vedhæft en motiveret ansøgning</label>
-<input type="file" name="frivillig_fil"> 
 </div>
 </fieldset>
 
 
-<fieldset>
+<fieldset id="frivillig_omraade">
 <legend>Hvilket område vil du arbejde med?</legend>
-<input type="radio" id="omraade_kommunikation" name="omraade_kommunikation" value="kommunikation">
+<input type="radio" id="omraade_kommunikation" name="omraade" value="kommunikation">
   <label for="omraade_kommunikation">Kommunikation</label><br>
-<input type="radio" id="omraade_booking" name="omraade_booking" value="booking">
+<input type="radio" id="omraade_booking" name="omraade" value="booking">
   <label for="omraade_program">Booking</label><br>
-<input type="radio" id="omraade_program" name="omraade_program" value="program">
+<input type="radio" id="omraade_program" name="omraade" value="program">
   <label for="omraade_program">Program</label><br>
-<input type="radio" id="omraade_sikkerhed" name="omraade_sikkerhed" value="sikkerged">
+<input type="radio" id="omraade_sikkerhed" name="omraade" value="sikkerged">
   <label for="omraade_sikkerhed">Sikkerhed</label><br>
+<label for="file">Vedhæft din motiverede ansøgning</label>
+<input type="file" name="frivillig_fil" id="frivillig_fil"> 
+
+<input type="submit" value="SEND" id="submit_frivillig_form">
 </fieldset>
-
-
-<input type="submit" value="Send">
 </form>
+</div>
+</section>
+
+<script>
+  const submitBtn = document.querySelector("#submit_frivillig_form");
+  submitBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    getValues(); 
+  });
+
+function getValues() {
+
+
+    // DATA minus files
+    const url = "https://kursfestival-884a.restdb.io/rest/frivillig"; 
+  const navn = document.querySelector("#frivillig_navn").value;
+  const email = document.querySelector("#frivillig_email").value;
+  const telefon =
+    document.querySelector("#frivillig_telefon").value;
+  const fil = document.querySelector("#frivillig_fil").value;
+  const omraade = document.querySelector("input[name=omraade]:checked").value;
+
+  const data = {
+      frivillignavn: navn, 
+      frivilligemail: email, 
+      frivilligtelefon: telefon, 
+      frivilligfil: fil, 
+      frivilligomraade: omraade,
+  }
+  console.log(data); 
+  post(data, url);
+
+
+
+}
+
+ function post(data, url) {
+     console.log(data); 
+
+    const headers = {
+  "Content-Type": "application/json",
+  "x-apikey": "619634edfc71545b0f5e090f",
+  "cache-control": "no-cache",
+};
+  const postData = JSON.stringify(data);
+  fetch(url, {
+    method: "post",
+    headers: headers,
+    body: postData,
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+
+</script>
