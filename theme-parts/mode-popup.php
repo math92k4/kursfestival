b3fb42a4236e650c663a3ca5ac0b020ec91009c7
Rasmus <rasm38h8@stud.kea.dk>
2021-11-29 12:46:51 +0100

    small changes to eco box

--- theme-parts/mode-popup.php
+++ theme-parts/mode-popup.php
@@ -1,5 +1,9 @@
 <div class="mode_pop_up">
-    <p>Hos Kurs festival vægter vi klimaet højt. Derfor har vi lavet 2 udgaver bla bla bla</p>
-    <button class="eco_btn">Eco mode</button>
-    <button class="normal_btn">Normal</button>
+    <p class="welcome">Velkommen til Kurs!</p>
+    <p>Hos Kurs Festival vægter vi klimaet høj og vi har derfor lavet to udgaver af vores hjemmeside. En som er grøn og en som er grønnere. På den grønnere har vi sparret på indholdet og derfor mindsket mængden af elektricitet der skal til for at vide siden. 
+       Du kan stadigvæk finde al relevant information om festivalen, men du kommer til at mangle billeder og videoer</p>
+    <div>
+        <button class="normal_btn">GRØN</button>
+        <button class="eco_btn">GRØNNERE</button>
+    </div>
 </div>
