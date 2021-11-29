2de5aad9d34d2b66f263eb3f1aad3556c63c12e0
Rasmus <rasm38h8@stud.kea.dk>
2021-11-29 12:55:02 +0100

    eco box made responsive

--- theme-parts/mode-popup.php
+++ theme-parts/mode-popup.php
@@ -1,6 +1,6 @@
 <div class="mode_pop_up">
-    <p class="welcome">Velkommen til Kurs!</p>
-    <p>Hos Kurs Festival vægter vi klimaet høj og vi har derfor lavet to udgaver af vores hjemmeside. En som er grøn og en som er grønnere. På den grønnere har vi sparret på indholdet og derfor mindsket mængden af elektricitet der skal til for at vide siden. 
+    <p class="welcome">Velkommen!</p>
+    <p>Hos Kurs Festival vægter vi klimaet høj og vi har derfor lavet to udgaver af vores hjemmeside. En som er grøn og en som er grønnere. På den grønnere udgave har vi sparret på indholdet og derfor mindsket mængden af elektricitet der skal til for at vide siden. 
        Du kan stadigvæk finde al relevant information om festivalen, men du kommer til at mangle billeder og videoer</p>
     <div>
         <button class="normal_btn">GRØN</button>
