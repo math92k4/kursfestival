906ade39eca5ac84a0f3826066ddb06a2a6bd388
Rasmus <rasm38h8@stud.kea.dk>
2021-11-29 13:01:18 +0100

    border around eco box and changes in text

--- theme-parts/mode-popup.php
+++ theme-parts/mode-popup.php
@@ -1,7 +1,7 @@
 <div class="mode_pop_up">
     <p class="welcome">Velkommen!</p>
-    <p>Hos Kurs Festival vægter vi klimaet høj og vi har derfor lavet to udgaver af vores hjemmeside. En som er grøn og en som er grønnere. På den grønnere udgave har vi sparret på indholdet og derfor mindsket mængden af elektricitet der skal til for at vide siden. 
-       Du kan stadigvæk finde al relevant information om festivalen, men du kommer til at mangle billeder og videoer</p>
+    <p>Hos Kurs Festival vægter vi klimaet høj og vi har derfor lavet to udgaver af vores hjemmeside. En som er grøn og en som er grønnere. På den grønnere udgave har vi sparret på indholdet og derfor mindsket mængden af elektricitet der skal til for at vise siden. 
+      <br> Du kan selfølgelig stadigvæk finde al relevant information om festivalen 🥰</p>
     <div>
         <button class="normal_btn">GRØN</button>
         <button class="eco_btn">GRØNNERE</button>
