406522fbd7791e56df6d485a9fa309ece7d34735
Mathias <math92k4@stud.kea.dk>
2021-12-01 09:35:55 +0100

    new sass

---
+++ oldphp/theme-parts/mode-popup.php
@@ -0,0 +1,9 @@
+<div class="mode_pop_up">
+    <p class="welcome">Velkommen!</p>
+    <p>Hos Kurs Festival er vi glade for klimaet. Meget glade. <br> Vi har derfor lavet to udgaver af vores hjemmeside. En som er grøn og en som er grønnere. På den grønnere udgave har vi sparret på indholdet og derfor betydeligt mindsket mængden af elektricitet der skal til for at vise siden. 
+      Du kan selfølgelig stadigvæk finde al relevant information om vores festival 🥰</p>
+    <div>
+        <button class="normal_btn">GRØN</button>
+        <button class="eco_btn">GRØNNERE</button>
+    </div>
+</div>
